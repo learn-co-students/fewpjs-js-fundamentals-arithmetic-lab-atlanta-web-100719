dd5869e534578ea21cdd74c4e0f1029784ab0111
clarkj99 <clark@stontech.net>
2019-11-24 22:08:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+
+var random = Math.random() * 10 + 1;
+
+var mod = 10 % 6;
+
+var max = Math.max(1, 3, 20, 5, 6)
